eadaf79e451b75aa0fbb92890ea01a23027c38ca
GayathriKaki <ghani319@gmail.com>
2019-11-29 12:43:48 +1100

    Add files via upload

--- KnockKnock.Common/Enums/ShapeType.cs
+++ KnockKnock.Common/Enums/ShapeType.cs
@@ -1,1 +1,25 @@
+﻿namespace KnockKnock.Common
+{
+    public enum ShapeType
+    {
+        /// <summary>
+        ///     The error
+        /// </summary>
+        Error = 0,
 
+        /// <summary>
+        ///     The equilateral
+        /// </summary>
+        Equilateral = 1,
+
+        /// <summary>
+        ///     The isosceles
+        /// </summary>
+        Isosceles = 2,
+
+        /// <summary>
+        ///     The scalene
+        /// </summary>
+        Scalene = 3
+    }
+}
