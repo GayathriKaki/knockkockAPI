e2d1953e8fd0ee4648106c37aeb7fe3775bbefdc
GayathriKaki <ghani319@gmail.com>
2019-11-29 12:41:20 +1100

    Add files via upload

--- KnockKnock.Api.Test/Fixture/TestClientContext.cs
+++ KnockKnock.Api.Test/Fixture/TestClientContext.cs
@@ -1,1 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
 
+namespace KnockKnock.Api.Test.Fixture
+{
+    public class TestClientContext : IDisposable
+    {
+        private TestServer _server;
+
+        public TestClientContext()
+        {
+            SetUpClient();
+        }
+
+        public HttpClient Client { get; private set; }
+
+        private void SetUpClient()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+
+            Client = _server.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            _server?.Dispose();
+            Client?.Dispose();
+        }
+    }
+}
