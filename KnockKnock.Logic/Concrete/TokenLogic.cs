5b857a04bd0aa7021c4b6be0e21045c14b291778
GayathriKaki <ghani319@gmail.com>
2019-11-29 12:47:26 +1100

    Add files via upload

---
+++ KnockKnock.Logic/Concrete/TokenLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace KnockKnock.Logic.Concrete
+{
+    public class TokenLogic : ITokenLogic
+    {
+        public TokenLogic(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private IConfiguration _configuration { get; }
+
+        public Guid GetToken()
+        {
+            return Guid.Parse(_configuration["MyToken"]);
+        }
+    }
+}
