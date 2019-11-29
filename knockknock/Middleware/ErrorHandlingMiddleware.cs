efd6b1b99f84f178382b9e7d3a13983f1ae6b6a3
GayathriKaki <ghani319@gmail.com>
2019-11-29 12:31:45 +1100

    Add files via upload

--- knockknock/Middleware/ErrorHandlingMiddleware.cs
+++ knockknock/Middleware/ErrorHandlingMiddleware.cs
@@ -1,1 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
+namespace KnockKnock.Api.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.BadRequest; // 500 if unexpected
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+
+            if (exception is ArgumentOutOfRangeException)
+            {
+                code = HttpStatusCode.NoContent;
+                return context.Response.WriteAsync(string.Empty);
+            }
+
+            var result = JsonConvert.SerializeObject(new { message = "The request is invalid." });
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
